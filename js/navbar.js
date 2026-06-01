bbdcebbefdc570eee45cc68e0fd1cb7df20f705e
arielfri669-cell <arielfri669@gmail.com>
2026-06-01 19:47:52 +0300

    new update

--- js/navbar.js
+++ js/navbar.js
@@ -1,2 +1,19 @@
 // navbar.js
-console.log("Navbar Loaded");
+
+console.log("Navbar Loaded")
+
+const navLinks = document.querySelectorAll(".nav-links a")
+
+function markActiveLink() {
+  const currentPage = window.location.pathname.split("/").pop()
+
+  navLinks.forEach((link) => {
+    const linkPage = link.getAttribute("href").split("/").pop()
+
+    if (linkPage === currentPage) {
+      link.classList.add("active-link")
+    }
+  })
+}
+
+markActiveLink()
