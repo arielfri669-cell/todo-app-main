3a188c3c47f552ffc0b6e3181b521ee203c54c37
arielfri669-cell <arielfri669@gmail.com>
2026-06-01 19:30:08 +0300

    finel style - ariel

--- js/navbar.js
+++ js/navbar.js
@@ -1,1 +1,19 @@
-console.log("Navbar Loaded");
+// navbar.js
+
+console.log("Navbar Loaded")
+
+const navLinks = document.querySelectorAll(".nav-links a")
+
+function markActiveLink() {
+  const currentPage = window.location.pathname.split("/").pop()
+
+  navLinks.forEach((link) => {
+    const linkPage = link.getAttribute("href").split("/").pop()
+
+    if (linkPage === currentPage) {
+      link.classList.add("active-link")
+    }
+  })
+}
+
+markActiveLink()
